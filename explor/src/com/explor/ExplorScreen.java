6727b500bb0fe2037ea15f2b9aaede875c32010e
Chris Dzombak <chris@chrisdzombak.net>
2011-09-28 22:07:33 -0400

    add working GPS monitoring code!

--- explor/src/com/explor/ExplorScreen.java
+++ explor/src/com/explor/ExplorScreen.java
@@ -1,13 +1,41 @@
 package com.explor;
 
 import android.app.Activity;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 
 public class ExplorScreen extends Activity {
-    /** Called when the activity is first created. */
+    
+	private static final String TAG = ExplorScreen.class.getSimpleName();
+	
+	private static final int    MIN_NOTIFICATION_DIST = 2; // meters
+	//private static final String LOCATION_PROVIDER     = LocationManager.NETWORK_PROVIDER;
+	private static final String LOCATION_PROVIDER     = LocationManager.GPS_PROVIDER;
+	
+	private LocationManager mLocationMgr;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        mLocationMgr = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        mLocationMgr.requestLocationUpdates(LOCATION_PROVIDER, 0, MIN_NOTIFICATION_DIST, mLocationListener);
     }
+
+    private final LocationListener mLocationListener = new LocationListener() {
+    	public void onLocationChanged(Location location) {
+    		Log.i(TAG, location.getLatitude() + "  " + location.getLongitude());
+    	}
+
+    	public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+    	public void onProviderEnabled(String provider) {}
+
+    	public void onProviderDisabled(String provider) {}
+    };
 }
